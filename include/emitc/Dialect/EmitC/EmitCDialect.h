c132b44dbd8b5aa75e4d695ff5c9cece1fd7b9ce
Marius Brehler <marius.brehler@iml.fraunhofer.de>
2020-07-27 11:14:59 +0200

    Adopt SCF YieldOp as EmitC_YieldOp

--- include/emitc/Dialect/EmitC/EmitCDialect.h
+++ include/emitc/Dialect/EmitC/EmitCDialect.h
@@ -16,6 +16,7 @@
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/OpDefinition.h"
 #include "mlir/IR/StandardTypes.h"
+#include "mlir/Interfaces/ControlFlowInterfaces.h"
 #include "mlir/Interfaces/SideEffectInterfaces.h"
 
 namespace mlir {

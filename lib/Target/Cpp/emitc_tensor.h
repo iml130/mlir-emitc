b7eb9ce323e4820e88c17d5e9c827fe920240b6c
Simon Camphausen <simon.camphausen@iml.fraunhofer.de>
2020-09-03 19:13:02 +0200

    Add Tensor class

---
+++ lib/Target/Cpp/emitc_tensor.h
@@ -0,0 +1,106 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// This file defines tensor classes used by EmitC
+
+#include <vector>
+
+template <typename T, size_t SIZE>
+class Tensor {
+public:
+  using ElementType = T;
+  using IteratorType = typename std::vector<T>::iterator;
+
+  Tensor() : data(SIZE) {}
+
+  Tensor(std::initializer_list<T> data) : data(data) {}
+
+  IteratorType begin() { return data.begin(); }
+
+  IteratorType end() { return data.end(); }
+
+  // Index into the flat data buffer.
+  T &operator[](size_t x) { return data[x]; }
+
+  std::vector<T> data;
+  static constexpr size_t size = SIZE;
+};
+
+template <typename T, size_t DimX>
+class Tensor1D : public Tensor<T, DimX> {
+public:
+  Tensor1D() : Tensor<T, DimX>() {}
+
+  Tensor1D(std::initializer_list<T> data) : Tensor<T, DimX>(data) {}
+
+  T &operator()(size_t x) { return this->data.at(x); }
+
+  static constexpr size_t dimX = DimX;
+};
+
+template <typename T, size_t DimX, size_t DimY>
+class Tensor2D : public Tensor<T, DimX * DimY> {
+public:
+  Tensor2D() : Tensor<T, DimX * DimY>() {}
+
+  Tensor2D(std::initializer_list<T> data) : Tensor<T, DimX * DimY>(data) {}
+
+  T &operator()(size_t x, size_t y) { return this->data.at(x * DimY + y); }
+
+  static constexpr size_t dimX = DimX;
+  static constexpr size_t dimY = DimY;
+};
+
+template <typename T>
+using is_scalar = std::is_arithmetic<T>;
+
+template <typename T>
+struct is_tensor_1d : std::false_type {};
+
+template <typename T, size_t DimX>
+struct is_tensor_1d<Tensor1D<T, DimX>> : std::true_type {};
+
+template <typename T>
+struct is_tensor_2d : std::false_type {};
+
+template <typename T, size_t DimX, size_t DimY>
+struct is_tensor_2d<Tensor2D<T, DimX, DimY>> : std::true_type {};
+
+template <typename T, typename Unused = void>
+struct is_tensor : std::false_type {};
+
+template <typename T>
+struct is_tensor<T,
+                 typename std::enable_if<std::is_base_of<
+                     Tensor<typename T::ElementType, T::size>, T>::value>::type>
+    : std::true_type {};
+
+template <typename T>
+using IsScalar = typename std::enable_if<std::is_scalar<T>::value, bool>::type;
+
+template <typename T>
+using IsTensor = typename std::enable_if<is_tensor<T>::value, bool>::type;
+
+template <typename T>
+struct get_element_type {
+  using type = T;
+};
+
+template <typename T, size_t DimX>
+struct get_element_type<Tensor1D<T, DimX>> {
+  using type = T;
+};
+
+template <typename T, size_t DimX, size_t DimY>
+struct get_element_type<Tensor2D<T, DimX, DimY>> {
+  using type = T;
+};
---
+++ unittests/emitc_tensor.cpp
@@ -0,0 +1,74 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "emitc_tensor.h"
+#include "emitc_mhlo.h"
+
+#include "gmock/gmock.h"
+
+using ::testing::ElementsAre;
+
+TEST(tensor, default_constructor_1d) {
+  Tensor1D<float, 2> tensor;
+
+  // multi dimensional indexing
+  EXPECT_EQ(0.0, tensor(0));
+  EXPECT_EQ(0.0, tensor(1));
+
+  // flat indexing
+  EXPECT_EQ(0.0, tensor[0]);
+  EXPECT_EQ(0.0, tensor[1]);
+}
+
+TEST(tensor, initializer_list_1d) {
+  Tensor1D<float, 2> tensor{1.0, 2.0};
+
+  // multi dimensional indexing
+  EXPECT_EQ(1.0, tensor(0));
+  EXPECT_EQ(2.0, tensor(1));
+
+  // flat indexing
+  EXPECT_EQ(1.0, tensor[0]);
+  EXPECT_EQ(2.0, tensor[1]);
+}
+
+TEST(tensor, default_constructor_2d) {
+  Tensor2D<float, 2, 2> tensor;
+
+  // multi dimensional indexing
+  EXPECT_EQ(0.0, tensor(0, 0));
+  EXPECT_EQ(0.0, tensor(0, 1));
+  EXPECT_EQ(0.0, tensor(1, 0));
+  EXPECT_EQ(0.0, tensor(1, 1));
+
+  // flat indexing
+  EXPECT_EQ(0.0, tensor[0]);
+  EXPECT_EQ(0.0, tensor[1]);
+  EXPECT_EQ(0.0, tensor[2]);
+  EXPECT_EQ(0.0, tensor[3]);
+}
+
+TEST(tensor, initializer_list_2d) {
+  Tensor2D<int32_t, 2, 2> tensor{1, 2, 3, 4};
+
+  // multi dimensional indexing
+  EXPECT_EQ(1, tensor(0, 0));
+  EXPECT_EQ(2, tensor(0, 1));
+  EXPECT_EQ(3, tensor(1, 0));
+  EXPECT_EQ(4, tensor(1, 1));
+
+  // flat indexing (row major)
+  EXPECT_EQ(1, tensor[0]);
+  EXPECT_EQ(2, tensor[1]);
+  EXPECT_EQ(3, tensor[2]);
+  EXPECT_EQ(4, tensor[3]);
+}

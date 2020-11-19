76e52e1369a2f404eb38c61d853717a514c6419d
Marius Brehler <marius.brehler@iml.fraunhofer.de>
2020-11-19 12:51:19 +0100

    Rename ConvertMHLOToStandard.cpp to MHLOToStd.cpp

--- lib/Transforms/MHLOToStd.cpp
+++ lib/Transforms/MHLOToStd.cpp
@@ -1,4 +1,4 @@
-//===- ConvertMHLOToStandard.cpp - MHLO const to std conversion -*- C++ -*-===//
+//===- MHLOToStd.cpp - MHLO const to std conversion -------------*- C++ -*-===//
 //
 // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.

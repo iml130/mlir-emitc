ec674a45e4da587831afa55aa2dd8603e569f225
Simon Camphausen <simon.camphausen@iml.fraunhofer.de>
2021-01-27 10:16:56 +0000

    Make 'batch-size' a command line argument.
    Also remove the unnecessary copy of the original Keras model.

--- scripts/e2e_test_keras.sh
+++ scripts/e2e_test_keras.sh
@@ -13,8 +13,8 @@
 
 set -e
 
-if [[ $# -ne 6 ]] ; then
-  echo "Usage: $0 <path/to/keras/model> <path/to/emitc/include/files> <path/to/emitc-opt> <compiler> <seed> <output_dir>"
+if [[ $# -ne 7 ]] ; then
+  echo "Usage: $0 <path/to/keras/model> <path/to/emitc/include/files> <path/to/emitc-opt> <compiler> <batch-size> <seed> <output_dir>"
   echo
   echo "This script expects a python version in the PATH with a recent version of tensorflow installed."
   echo "Tested with python 3.6.9 and tensorflow 2.4.0" 
@@ -27,27 +27,24 @@ EMITC_INCLUDE_DIR=$2
 EMITC_OPT=$3
 EMITC_TRANSLATE=$(dirname $EMITC_OPT)/emitc-translate
 CPP_COMPILER=$4
-SEED=$5
-BATCH_SIZE=2
-OUTPUT_DIR=$6
+BATCH_SIZE=$5
+SEED=$6
+OUTPUT_DIR=$7
 
 echo "KERAS_MODEL=$KERAS_MODEL"
 echo "EMITC_INCLUDE_DIR=$EMITC_INCLUDE_DIR"
 echo "EMITC_OPT=$EMITC_OPT"
 echo "EMITC_TRANSLATE=$EMITC_TRANSLATE"
 echo "CPP_COMPILER=$CPP_COMPILER"
-echo "SEED=$SEED"
 echo "BATCH_SIZE=$BATCH_SIZE"
+echo "SEED=$SEED"
 echo "OUTPUT_DIR=$OUTPUT_DIR"
 
 echo "Setting up output directory"
 mkdir -p "$OUTPUT_DIR"
 
-echo "Copying over keras model"
-cp "$KERAS_MODEL" "$OUTPUT_DIR"/model.h5
-
 echo "Converting keras model to saved model format"
-python keras_to_savedmodel.py --batch-size $BATCH_SIZE "$KERAS_MODEL" "$OUTPUT_DIR"/model
+python keras_to_savedmodel.py --batch-size "$BATCH_SIZE" "$KERAS_MODEL" "$OUTPUT_DIR"/model
 
 echo "Translating saved model to tf dialect"
 EXPORTED_NAME=predict
@@ -64,9 +61,7 @@ echo "Canonicalizing mhlo dialect"
 
 echo "Fixing function name"
 FUNCTION_NAME=$(grep -oe "@[^(]*" "$OUTPUT_DIR"/model_canon.mlir)
-# Drop leading @ symbol
-FUNCTION_NAME="${FUNCTION_NAME:1}"
-sed "s/$FUNCTION_NAME/predict/g" "$OUTPUT_DIR"/model_canon.mlir > "$OUTPUT_DIR"/model_fix_name.mlir
+sed "s/$FUNCTION_NAME/@predict/g" "$OUTPUT_DIR"/model_canon.mlir > "$OUTPUT_DIR"/model_fix_name.mlir
 
 echo "Converting mhlo dialect to emitc dialect"
 "$EMITC_OPT" --mhlo-control-flow-to-scf --convert-mhlo-region-ops-to-emitc --convert-mhlo-to-emitc --convert-scf-to-emitc --convert-std-to-emitc --convert-mhlo-const-to-std "$OUTPUT_DIR"/model_fix_name.mlir > "$OUTPUT_DIR"/model_emitc.mlir 
@@ -75,7 +70,7 @@ echo "Translating emitc dialect to cpp header"
 "$EMITC_TRANSLATE" --mlir-to-cpp "$OUTPUT_DIR"/model_emitc.mlir > "$OUTPUT_DIR"/model_generated.h
 
 echo "Generating test case"
-python generate_testscases.py --file-format cpp --count 1 --batch-size $BATCH_SIZE --seed $SEED "$KERAS_MODEL" "$OUTPUT_DIR"
+python generate_testscases.py --file-format cpp --count 1 --batch-size "$BATCH_SIZE" --seed "$SEED" "$KERAS_MODEL" "$OUTPUT_DIR"
 
 echo "Compiling test case"
 "$CPP_COMPILER" "$OUTPUT_DIR"/test.cpp -O3 -I "$EMITC_INCLUDE_DIR" -I "$OUTPUT_DIR" -o "$OUTPUT_DIR"/test

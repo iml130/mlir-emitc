24cefec95fbf0f34cb64e186a99c32cd7b1e1429
Oliver Scherf <oliver.scherf@iml.fraunhofer.de>
2021-03-31 13:45:44 +0200

    Add conversion for tosa.pow

--- include/emitc/emitc_core_ops.h
+++ include/emitc/emitc_core_ops.h
@@ -179,6 +179,36 @@ inline Src mul(Src x, Src y) {
   return binary<Src>(x, y, f);
 }
 
+// PowOp
+template <typename Src>
+inline Src pow(Src x, Src y) {
+  using ET_Src = typename get_element_type<Src>::type;
+
+  auto f = [](ET_Src a, ET_Src b) -> ET_Src {
+    if (std::is_integral<ET_Src>::value) {
+      const bool negative = b < 0;
+      if (b < 0) {
+        b = -b;
+      }
+
+      ET_Src result = 1;
+
+      for (ET_Src i = 0; i < b; i++) {
+        result *= a;
+      }
+
+      if (negative) {
+        result = 1 / result;
+      }
+      return result;
+    } else {
+      return std::pow(a, b);
+    }
+  };
+
+  return binary<Src>(x, y, f);
+}
+
 // SubOp
 template <typename Src>
 inline Src sub(Src x, Src y) {

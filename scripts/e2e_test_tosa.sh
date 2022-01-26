ff56ddfc69a35fa7c4dce0984d742b48dbc7cd25
Henri Gründer <henri.gruender@iml.fraunhofer.de>
2022-01-26 12:22:19 +0100

    Remove tf-opt dependency from tosa e2e script (#255)
    Co-authored-by: Benjamin Heidebroek <benjamin.heidebroek@iml.fraunhofer.de>
    Co-authored-by: Marius Brehler <marius.brehler@iml.fraunhofer.de>

--- scripts/e2e_test_tosa.sh
+++ scripts/e2e_test_tosa.sh
@@ -13,27 +13,25 @@
 
 set -e
 
-if [[ $# -ne 8 ]] ; then
-  echo "Usage: $0 <path/to/model> <path/to/emitc/reference-implementation/include/> <path/to/tf-opt> <path/to/emitc-opt> <compiler> <batch-size> <seed> <output_dir>"
+if [[ $# -ne 7 ]] ; then
+  echo "Usage: $0 <path/to/model> <path/to/emitc/reference-implementation/include/> <path/to/emitc-opt> <compiler> <batch-size> <seed> <output_dir>"
   echo
   echo "Both a keras and a tensorflow saved model is supported."
   echo
   echo "This script expects a python version in the PATH with a recent version of tensorflow installed."
-  echo "You need to compile tensorflow from source to build the tf-opt program."
-  echo "Tested with python 3.6.9 and tensorflow 2.4.0"
+  echo "Tested with python 3.8.10 and tf-nightly 2.8.0.dev20211102"
 
   exit 1
 fi
 
 MODEL=$1
 EMITC_INCLUDE_DIR=$2
-TF_OPT=$3
-EMITC_OPT=$4
+EMITC_OPT=$3
 EMITC_TRANSLATE=$(dirname $EMITC_OPT)/emitc-translate
-CPP_COMPILER=$5
-BATCH_SIZE=$6
-SEED=$7
-OUTPUT_DIR=$8
+CPP_COMPILER=$4
+BATCH_SIZE=$5
+SEED=$6
+OUTPUT_DIR=$7
 
 echo "MODEL=$MODEL"
 echo "EMITC_INCLUDE_DIR=$EMITC_INCLUDE_DIR"
@@ -58,14 +56,14 @@ echo "Optimizing tf dialect"
 python optimize_tf_dialect.py "$OUTPUT_DIR"/model_tf.mlir "$OUTPUT_DIR"/model_tf_opt.mlir
 
 echo "Converting tf dialect to tosa dialect"
-"$TF_OPT" --tf-to-tosa-pipeline "$OUTPUT_DIR"/model_tf_opt.mlir > "$OUTPUT_DIR"/model_tosa.mlir
+python tf_to_tosa_dialect.py "$OUTPUT_DIR"/model_tf_opt.mlir "$OUTPUT_DIR"/model_tosa.mlir
 
 echo "Removing tf._input_shapes attribute"
 sed "s/tf._input_shapes =.*]//" "$OUTPUT_DIR"/model_tosa.mlir > "$OUTPUT_DIR"/model_tosa_noattr.mlir
 sed -i "s/, }/}/" "$OUTPUT_DIR"/model_tosa_noattr.mlir
 
 echo "Fixing function name"
-FUNCTION_NAME=$(grep -oe "@[^(]*" "$OUTPUT_DIR"/model_tosa_noattr.mlir)
+FUNCTION_NAME=$(grep -m 1 -oe "@[^(]*" "$OUTPUT_DIR"/model_tosa_noattr.mlir)
 sed "s/$FUNCTION_NAME/@predict/g" "$OUTPUT_DIR"/model_tosa_noattr.mlir > "$OUTPUT_DIR"/model_fix_name.mlir
 
 echo "Converting tosa dialect to emitc dialect"

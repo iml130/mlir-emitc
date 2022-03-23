68cdb4f3221b4e82bdf8b0aeec7ed1caa269a052
Marius Brehler <marius.brehler@iml.fraunhofer.de>
2022-03-23 10:11:19 +0100

    Integrate LLVM at llvm/llvm-project@ca47011 (#284)
    Updates LLVM usage to match llvm/llvm-project@ca47011. This adopts to
    the move of FuncOp.
    Further updates the MLIR-HLO submodule to tensorflow/mlir-hlo@a8337ce.

--- lib/Dialect/EmitC/Conversion/PassDetail.h
+++ lib/Dialect/EmitC/Conversion/PassDetail.h
@@ -9,6 +9,7 @@
 #ifndef DIALECT_EMITC_CONVERSION_PASSDETAIL_H
 #define DIALECT_EMITC_CONVERSION_PASSDETAIL_H
 
+#include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Pass/Pass.h"
 
 namespace mlir {

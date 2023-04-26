8c9832cdc32aab11f7cd2e52948a25be88fca40c
Marius Brehler <marius.brehler@iml.fraunhofer.de>
2023-04-26 15:34:46 +0000

    Add Copyright Notices
    Add copyright notices to the reference implementation for the case of
    re-licensing.
    
    As part of https://reviews.llvm.org/D146483, part of the reference
    implementation is forked. Even if not as part of this pull request, it
    might be of interest to change the license from `Apache-2.0` to
    `Apache-2.0 WITH LLVM-exception`. The copyright notices can help to
    facilitate this.

--- reference-implementation/unittests/tosa.cpp
+++ reference-implementation/unittests/tosa.cpp
@@ -1,3 +1,7 @@
+// Copyright Fraunhofer-Gesellschaft zur Förderung der angewandten
+//           Forschung e.V.
+// Copyright Google LLC
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
